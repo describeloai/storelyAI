72ff4dfa79f87360d168178f9d90b295822de381
describeloai <victorgvobregon@gmail.com>
2025-06-06 12:45:40 +0200

    Deploy to Vercel

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,30 @@
-'use client'
-import dynamic from 'next/dynamic'
+'use client';
+import { useEffect, useState } from 'react';
+import { useRouter, useSearchParams } from 'next/navigation';
+import dynamic from 'next/dynamic';
 
 const Landing = dynamic(() => import('./landing/LandingLayout'), {
   ssr: false,
-}) as React.ComponentType
+}) as React.ComponentType;
 
 export default function EntryPoint() {
-  return <Landing />
+  const router = useRouter();
+  const params = useSearchParams();
+  const [shouldShowLanding, setShouldShowLanding] = useState(true);
+
+  useEffect(() => {
+    const shop = params.get('shop');
+    const host = params.get('host');
+    const isEmbedded = params.get('embedded');
+
+    if (shop && host && isEmbedded === '1') {
+      const url = `/dashboard?shop=${shop}&host=${host}&embedded=1`;
+      router.replace(url);
+      setShouldShowLanding(false);
+    }
+  }, [params, router]);
+
+  if (!shouldShowLanding) return null;
+
+  return <Landing />;
 }
