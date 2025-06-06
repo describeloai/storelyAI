e72c445f5cb8756e94a50e71427ba3595ee616d9
describeloai <victorgvobregon@gmail.com>
2025-06-06 13:28:01 +0200

    Deploy to Vercel

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,22 @@
 'use client';
-import { Suspense } from 'react';
-import EntryPointRouter from './EntryPointRouter';
+import { Suspense, useState } from 'react';
 import dynamic from 'next/dynamic';
+import EntryPointRouter from './EntryPointRouter';
 
 const Landing = dynamic(() => import('./landing/LandingLayout'), {
   ssr: false,
 }) as React.ComponentType;
 
 export default function EntryPointPage() {
+  const [showLanding, setShowLanding] = useState(true);
+
   return (
     <>
-      <Suspense fallback={<p>Redireccionando...</p>}>
-        <EntryPointRouter />
+      <Suspense fallback={<p>Redirigiendo...</p>}>
+        <EntryPointRouter onRedirect={() => setShowLanding(false)} />
       </Suspense>
-      <Landing />
+
+      {showLanding && <Landing />}
     </>
   );
 }
